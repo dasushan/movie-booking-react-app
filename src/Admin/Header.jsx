ba70d6e590d881d4795f8ac0d48c87437a1009a7
dasushan <53913896+dasushan@users.noreply.github.com>
2024-11-30 02:28:10 +0530

    creating a minimilastic header

--- src/Admin/Header.jsx
+++ src/Admin/Header.jsx
@@ -1,4 +1,21 @@
+import { AlignJustify, UserRoundCheck } from 'lucide-react';
 const Header = () => {
-
-}
-export default Header
+  return (
+    <div className="bg-slate-100 w-[100%] h-[4rem] p-2 m-1">
+      <div className="flex justify-between items-center ">
+        <div className="flex gap-9 items-center p-1">
+          <div>
+            <AlignJustify />
+          </div>
+          <div>
+            <h1 className="text-xl font-bold">Admin Dashboard</h1>
+          </div>
+        </div>
+        <div className='mr-2 px-3'>
+          <UserRoundCheck />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Header;
